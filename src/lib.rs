0d18507e1807e72bd99dcbd75c04bffd9a4c8c97
Sridaran Thoniyil <sri7thon@gmail.com>
2021-03-09 11:58:17 -0500

    Create library module with Block struct

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+use chrono::prelude::*;
+use std::fmt;
+
+pub struct Block {
+    start_time: NaiveTime,
+    end_time: NaiveTime,
+
+    // caches this value rather than recalculating each time
+    length: chrono::Duration,
+}
+
+impl Block {
+    pub fn new(start_time: NaiveTime, end_time: NaiveTime) -> Block {
+        Block {
+            start_time,
+            end_time,
+            length: end_time.signed_duration_since(start_time),
+        }
+    }
+
+    pub fn contains(&self, time: &NaiveTime) -> bool {
+        self.end_time.signed_duration_since(*time) < self.length
+    }
+}
+
+impl fmt::Display for Block {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            formatter,
+            "{} - {}",
+            self.start_time.format("%H:%M"),
+            self.end_time.format("%H:%M")
+        )
+    }
+}
