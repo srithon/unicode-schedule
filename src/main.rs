fdb6ca1ff132306699ebf2258c988df869590912
Sridaran Thoniyil <sri7thon@gmail.com>
2021-03-09 13:19:06 -0500

    Use unicode_prettytable to display schedule

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,46 @@
 use chrono::prelude::*;
 use Weekday::*;
 
+use unicode_prettytable::*;
+
 use show_schedule;
 
 fn main() {
     let local = Local::now();
     let day = local.weekday();
 
-    let day_string = local.format("%A");
-
     use show_schedule::schedules::*;
-    let schedule_vector = match day {
-        Mon | Tue => [FULL_SCHEDULE_BASE, AFTERNOON_SCHEDULE_C].concat(),
-        Wed => [HALF_DAY_SCHEDULE].concat(),
-        Thu | Fri => [FULL_SCHEDULE_BASE, AFTERNOON_SCHEDULE_D].concat(),
-        _ => {
-            println!("You don't have school today! Relax!");
-            return;
-        }
+    let schedule = {
+        let base_blocks = match day {
+            Mon | Tue => [FULL_SCHEDULE_BASE, AFTERNOON_SCHEDULE_C].concat(),
+            Wed => [HALF_DAY_SCHEDULE].concat(),
+            Thu | Fri => [FULL_SCHEDULE_BASE, AFTERNOON_SCHEDULE_D].concat(),
+            _ => {
+                println!("You don't have school today! Relax!");
+                return;
+            }
+        };
+
+        let formatted_blocks = {
+            let blocks_iterator = base_blocks.into_iter().map(|row| row.to_vec()).collect::<Vec<_>>();
+
+            vec![vec!["Name", "Time"]].into_iter().chain(blocks_iterator).collect::<Vec<_>>()
+        };
+
+        formatted_blocks
     };
 
-    println!("{}\n{:#?}", day_string, schedule_vector)
+    let table = unicode_prettytable::TableBuilder::default()
+        .header(
+            HeaderBuilder::default()
+                .double_bar(true)
+                .centered_text(true)
+                .build()
+                .unwrap()
+        )
+        .rows(&schedule)
+        .build()
+        .unwrap();
+
+    println!("{}", table)
 }
