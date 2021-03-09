840c8de713c2c3b3f8ffa013433b4572039de065
Sridaran Thoniyil <sri7thon@gmail.com>
2021-03-09 13:27:59 -0500

    Revert "Pad times with spaces"
    This reverts commit d106a3e63b9156ac9e1cc35d013d52a20dd33e2a.

--- src/schedules.rs
+++ src/schedules.rs
@@ -1,24 +1,24 @@
 /// Common morning schedule for full-length days
 pub static FULL_SCHEDULE_BASE: &[&[&'static str; 2]] = &[
-    &["Block 1", " 7:30 -  8:36"],
-    &["Block 2", " 8:44 -  9:44"],
-    &["Block 3", " 9:52 - 10:52"],
+    &["Block 1", "7:30 - 8:36"],
+    &["Block 2", "8:44 - 9:44"],
+    &["Block 3", "9:52 - 10:52"],
     &["Block 4", "11:00 - 12:00"],
-    &["Lunch", "12:00 -  1:00"],
+    &["Lunch", "12:00 - 1:00"],
 ];
 
 /// Repeats blocks 1 and 2 in the afternoon
 pub static AFTERNOON_SCHEDULE_C: &[&[&'static str; 2]] =
-    &[&["Block 1", " 1:00 -  1:25"], &["Block 2", " 1:30 -  1:55"]];
+    &[&["Block 1", "1:00 - 1:25"], &["Block 2", "1:30 - 1:55"]];
 
 /// Repeats blocks 3 and 4 in the afternoon
 pub static AFTERNOON_SCHEDULE_D: &[&[&'static str; 2]] =
-    &[&["Block 3", " 1:00 -  1:25"], &["Block 4", " 1:30 -  1:55"]];
+    &[&["Block 3", "1:00 - 1:25"], &["Block 4", "1:30 - 1:55"]];
 
 /// No afternoon session
 pub static HALF_DAY_SCHEDULE: &[&[&'static str; 2]] = &[
-    &["Block 1", " 7:30 -  8:30"],
-    &["Block 2", " 8:40 -  9:40"],
-    &["Block 3", " 9:50 - 10:50"],
+    &["Block 1", "7:30 - 8:30"],
+    &["Block 2", "8:40 - 9:40"],
+    &["Block 3", "9:50 - 10:50"],
     &["Block 4", "11:00 - 12:00"],
 ];
