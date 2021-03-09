956fd9c863cdd86fb9fdaa08a738346a9f3a1ac2
Sridaran Thoniyil <sri7thon@gmail.com>
2021-03-09 12:37:45 -0500

    "Pretty-print" relevant schedule in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use chrono::prelude::*;
+use Weekday::*;
+
+use show_schedule;
+
 fn main() {
-    println!("Hello, world!");
+    let local = Local::now();
+    let day = local.weekday();
+
+    let day_string = local.format("%A");
+
+    use show_schedule::schedules::*;
+    let schedule_vector = match day {
+        Mon | Tue => [FULL_SCHEDULE_BASE, AFTERNOON_SCHEDULE_C].concat(),
+        Wed => [HALF_DAY_SCHEDULE].concat(),
+        Thu | Fri => [FULL_SCHEDULE_BASE, AFTERNOON_SCHEDULE_D].concat(),
+        _ => {
+            println!("You don't have school today! Relax!");
+            return;
+        }
+    };
+
+    println!("{}\n{:#?}", day_string, schedule_vector)
 }
